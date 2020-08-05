3f7d6ba5c52e64dc54db41bafd771e133085574c
Aleksei Raishin <ighn215@gmail.com>
2020-08-06 03:57:06 +0700

    Initial commit

---
+++ src/hospital/Main.java
@@ -0,0 +1,28 @@
+package hospital;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
+        primaryStage.getIcons().add(new Image("hospital/resources/icon.png"));
+        primaryStage.setTitle("Управление персоналом больницы");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+class FileCreator {
+
+}
